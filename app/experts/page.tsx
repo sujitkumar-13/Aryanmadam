26fbce180f97a55416101c5b561dcb3dd95da8c9
sujitkumar-13 <sujitkumar19013@gmail.com>
2026-02-25 11:16:40 +0530

    Remove the contact page and its associated API route.

--- app/experts/page.tsx
+++ app/experts/page.tsx
@@ -1,18 +1,18 @@
 // import AboutUs from "@/components/AboutUs";  // Function name 'About' hai
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Experts  from "@/components/TalkToExperts";
+import Experts from "@/components/TalkToExperts";
 import FloatingWhatsApp from "@/components/FloatingWhatsApp";
 import ReviewsFloating from "@/components/ReviewsFloating";
 
 export default function AboutPage() {
   return (
     <>
-       <Navbar />
-      <Experts/>
-     <Footer /> 
-     <FloatingWhatsApp /> {/* Floating WhatsApp Button */}
-     <ReviewsFloating />
+      <Navbar />
+      <Experts />
+      <Footer />
+      <FloatingWhatsApp /> {/* Floating WhatsApp Button */}
+      <ReviewsFloating />
     </>
   );
 }
